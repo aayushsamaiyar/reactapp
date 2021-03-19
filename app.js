f8a233356e4248455d7208b967cbe7a7454eca96
Aayush Samaiyar <68498029+aayushsamaiyar@users.noreply.github.com>
2021-03-19 23:03:35 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import { Container ,AppBar ,Typography, Grow, Grid} from '@material-ui/core';
+
+import memories from './images/memories.png';
+import Posts from './components/Posts/Posts';
+import Form from './components/Form/Form';
+const App = () => {
+    return(
+        <Container maxwidth='lg'>
+            <AppBar position='static' color='inherit'>
+                <Typography variant="h2" align="center">
+                    MEMORIES
+                </Typography>
+                <img src={memories} alt="memories" height="600">
+            </AppBar>
+            <Grow in>
+                <Container>
+                    <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+                        <Grid item xs = {12} sm ={7}>
+                            <Posts>
+                        </Grid>  
+                        <Grid>
+                            <Form>
+                        </Grid>  
+                    </Grid>
+                </Container>
+            </Grow>
+        </Container>
+    );
+}
+
+export default App;
